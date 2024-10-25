2a351c300d72689936f2cd33510261e26e5bd4ae
zerocho <zerohch0@gmail.com>
2024-10-25 20:22:52 +0900

    add: simple factory

---
+++ src/ChromeGrimpan.ts
@@ -0,0 +1,20 @@
+class ChromeGrimpan {
+  private static instance: ChromeGrimpan;
+  private constructor(canvas: HTMLElement | null) {
+    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+      throw new Error('canvas 엘리먼트를 입력하세요');
+    }
+  }
+
+  intialize() {}
+  intializeMenu() {}
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new ChromeGrimpan(document.querySelector('canvas'))
+    }
+    return this.instance;
+  }
+}
+
+export default Grimpan;
--- src/IEGrimpan.ts
+++ src/IEGrimpan.ts
@@ -1,5 +1,5 @@
-class Grimpan {
-  private static instance: Grimpan;
+export default class IEGrimpan {
+  private static instance: IEGrimpan;
   private constructor(canvas: HTMLElement | null) {
     if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
       throw new Error('canvas 엘리먼트를 입력하세요');
@@ -11,10 +11,8 @@ class Grimpan {
 
   static getInstance() {
     if (!this.instance) {
-      this.instance = new Grimpan(document.querySelector('canvas'))
+      this.instance = new IEGrimpan(document.querySelector('canvas'))
     }
     return this.instance;
   }
 }
-
-export default Grimpan;
